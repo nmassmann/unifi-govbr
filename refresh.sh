03cb6a5e0c0017b60e28b41e792fbc67961bc849
felipe volpato <felipe.volpato@MacBook-Pro-de-felipe.local>
2025-08-27 15:01:13 -0300

    atualizando script refresh

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Primeiro instalamos o composer
+OS=$(uname)
+if [ "$OS" = "Darwin" ]; then
+    echo "Instalando composer no macOS"
+    brew install composer
+elif [ "$OS" = "Linux" ]; then
+    echo "Instalando composer no Linux"
+    curl -s https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+else
+    echo "Sistema operacional não identificado: $OS"
+    exit
+fi
+
 rm -rf var/cache/*
 composer clearcache
 composer install --no-dev --optimize-autoloader --classmap-authoritative
